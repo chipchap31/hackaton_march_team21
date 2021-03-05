79ead5065b6e0c58b08a2250ec4fa6fb13359ad0
Gilleece <seangilleece@gmail.com>
2021-03-05 17:12:41 +0000

    Cards now dynamically generate using json data from user submissions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 function generateCards() {
-    $("#parade-cards").append(`
-        <div class="carousel-item">
-            <div class="d-flex align-items-center justify-content-center min-vh-100">
-                <div class="card" style="width: 60%;">
-                    <img src="images/flower.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text text-center">John Smith from America says: How did I get here?</p>
+    $(document).ready(function () {
+        $.getJSON("parade.json", function (data) {
+            for (i = 0; i < data.floats.length; i++) {
+                $("#parade-cards").append(`
+                <div class="carousel-item">
+                    <div class="d-flex align-items-center justify-content-center min-vh-100">
+                        <div class="card" style="width: 60%;">
+                            <img src="${data.floats[i].image}" class="card-img-top" alt="${data.floats[i].name}'s image">
+                            <div class="card-body">
+                                <p class="card-text text-center">${data.floats[i].name} from ${data.floats[i].country} says: ${data.floats[i].message}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    `)}
+            `)
+        }
+        console.log(data.floats[0])
+        }).fail(function () {
+            console.log("An error has occurred.");
+        });
+    });
+}
