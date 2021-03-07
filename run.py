c6d5f52e24c3159521d9f6ba0727f828d65a6886
Jomari Alang <jomarialang31@gmail.com>
2021-03-07 01:49:45 +0000

    fix: force debug to false

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from app import create_app
-
+import os
 
 app = create_app()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=os.environ.get('IP'), 
+    port=int(os.environ.get('PORT') or 5000),
+    debug=bool(os.getenv('DEBUG'))
+    )
 
