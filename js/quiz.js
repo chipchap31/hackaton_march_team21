ca8e61e8794626b4834ccd4e86468426626e6175
dovi-k <dovylee@gmail.com>
2021-03-07 19:58:51 +0000

    Added aditional questions

--- js/quiz.js
+++ js/quiz.js
@@ -78,37 +78,101 @@ function clearStatusClass(element) {
 
 const questions = [
   {
-    question: 'Who is St. Patrick?',
+    question: 'People traditionally wear green on St. Patrick`s Day to avoid what?',
     answers: [
-      { text: 'Famous Leprechaun', correct: false },
-      { text: 'Saint of Ireland ', correct: true },
-      { text: 'A jolly green man', correct: false},
-      { text: 'Apostle', correct: false}
+      { text: 'Getting kissed', correct: false },
+      { text: 'Getting pinched ', correct: true },
+      { text: 'One year of bad luck', correct: false},
+      { text: 'Getting mugged', correct: false}
     ]
   },
   {
-    question: 'When is St Patricks day?',
+    question: 'What nationality was St. Patrick?',
     answers: [
-      { text: '17th of March', correct: true },
-      { text: 'Everyday', correct: false },
-      { text: '25th of December', correct: false },
-      { text: '17th of April', correct: false }
+      { text: 'Irish', correct: false },
+      { text: 'German', correct: false },
+      { text: 'Scottish', correct: true },
+      { text: 'British', correct: false }
     ]
   },
   {
-    question: 'What miracles did St. Patrick Perform?',
+    question: 'What did St. Patrick believe a shamrock represented?',
     answers: [
-      { text: 'Gathered all the snake in Ireland', correct: false },
-      { text: 'Banished all the snaked from Ireland', correct: true },
-      { text: 'Walked on water', correct: false },
+      { text: 'Good luck', correct: false },
+      { text: 'Good fortune', correct: false },
+      { text: 'The holy trinity', correct: true },
       { text: 'IDK', correct: false }
     ]
   },
   {
-    question: 'What is the colour of St. Patricks day?',
+    question: 'Which U.S. city dyes its river green annually to celebrate St. Patrick`s Day?',
     answers: [
-      { text: 'Red', correct: false },
-      { text: 'Green', correct: true }
+      { text: 'Boston', correct: false },
+      { text: 'Detroid', correct: false },
+      { text: 'New York City', correct: false },
+      { text: 'Chicago', correct: true }
     ]
-  }
+  },
+    {
+    question: 'According to myth, when is the best time to sneak up on a leprechaun?',
+    answers: [
+      { text: 'When he is taking a nap', correct: false },
+      { text: 'When he is counting gold', correct: false },
+      { text: 'While he is mending his shoes', correct: true },
+      { text: 'During his supper', correct: false }
+    ]
+  },
+  {
+    question: 'What was St. Patrick`s given birth name?',
+    answers: [
+      { text: 'Maewyn Succat', correct: true },
+      { text: 'Henry O Malley', correct: false },
+      { text: 'Patrick O Riley', correct: false },
+      { text: 'John Smith', correct: false }
+    ]
+  },
+  {
+      question: 'How many pints of Guinness are consumed worldwide on St. Patrick`s Day?',
+    answers: [
+      { text: '13 million', correct: true },
+      { text: '10 million', correct: false },
+      { text: '5 million', correct: false },
+      { text: '8 million', correct: false }
+    ]
+  },
+   {
+      question: 'Where and when was the first St. Paddy’s Day parade in the United States?',
+    answers: [
+      { text: 'Boston in 1809', correct: false },
+      { text: 'Providence, R.I., in 1898', correct: false },
+      { text: 'New York in 1762', correct: true }
+    ]
+  },
+    {
+      question: 'How do Leprechauns earn their gold?',
+    answers: [
+      { text: 'Collecting teeth', correct: false },
+      { text: 'Making shoes', correct: true },
+      { text: 'Dancing', correct: false },
+      { text: 'Picking pockets', correct: false }
+    ]
+  },
+    {
+      question: 'Saint Patrick`s Day commemorates the _____ of Saint Patrick.',
+    answers: [
+      { text: 'Birth', correct: true },
+      { text: 'Coronation', correct: false },
+      { text: 'Death', correct: true },
+    ]
+  },
+    {
+      question: 'How long was the world`s shortest Saint Patrick`s Day parade?',
+    answers: [
+      { text: '23 meters', correct: true },
+      { text: '89 meters', correct: false },
+      { text: '10 meters', correct: false },
+      { text: '60 meters', correct: false }
+    ]
+  },
+
 ]
