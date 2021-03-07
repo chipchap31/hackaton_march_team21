d7172f9fb66e4368ca46cf1b77064206281482f4
Jomari Alang <jomarialang31@gmail.com>
2021-03-07 04:07:04 +0000

    feat: update parade model image value to bucket image url

--- app/views/parade_view.py
+++ app/views/parade_view.py
@@ -1,13 +1,14 @@
-from flask import Blueprint,render_template, jsonify, flash
+from flask import Blueprint, json,render_template, jsonify, flash, make_response
 from flask.globals import request
-from flask.helpers import make_response
+from flask.wrappers import Response
+from werkzeug.utils import redirect
 from ..models.parade_model import ParadeModel
 from ..services.s3bucket import S3Bucket
 from flask_mail import Message
 from ..Mailer import mail
 from itsdangerous import URLSafeSerializer
 import os
-
+import json
 
 
 serializer = URLSafeSerializer(os.getenv('SECRET_KEY'))
@@ -64,56 +65,49 @@ def parade_confirm(token):
     
     
     data = serializer.loads(token, salt='email-confirm')
-    try: 
+  
+ 
         
-        data_id = ParadeModel().create({**data})
-        
-        resp = make_response(data, 200)
+    data_id = ParadeModel().create(data_to_insert=data)
+    
+    if data_id:
 
-        resp.set_cookie('parade_id', data_id)
+        resp = make_response(render_template('parade-confirmed.html'), 200)
+
+        resp.set_cookie('parade_id', str(data_id).encode())
 
         return resp
-    except Exception as e:
-        flash(e)    
+    else: 
 
-        return jsonify({'Error': 'dsads'}),404
+        return Response('You already have an account',500)
     
 
 
 @parade_view.route('/parade/fetch')
 def parade_fetch():
+    
+   
     parade = {
-    "floats": [
-        {
-        "name": "John Doe",
-        "country": "Ireland",
-        "image": "https://www.irishtimes.com/polopoly_fs/1.3824661.1552497679!/image/image.jpg_gen/derivatives/box_1200_630/image.jpg",
-        "message": "Happy St Patrick's Day!"      
-        },
-        {
-        "name": "Mary Smith",
-        "country": "Germany",
-        "image": "https://www.shamrockeradventures.com/media/14828/st-patrciks-day-tours-mobile.jpg?anchor=center&mode=crop&rnd=132192357100000000",
-        "message": "Have a great day!"      
-        },
-        {
-        "name": "Supervalu",
-        "country": "Ireland",
-        "image": "https://cf-images.eu-west-1.prod.boltdns.net/v1/jit/4221396001/f7b10f7b-3a37-4b3a-8659-8e8438b39921/main/1280x720/59s466ms/match/image.jpg",
-        "message": "Greetings from Supervalu"      
-        },
-        {
-        "name": "Egg",
-        "country": "USA",
-        "image": "https://pbs.twimg.com/profile_images/482980960258908160/XawV_qBz_400x400.jpeg",
-        "message": "It's a jumping off point."      
-        },
-        {
-        "name": "Sweet Dee",
-        "country": "Canada",
-        "image": "https://external-preview.redd.it/8Rzj3TN_BaZ5rqoWGz799K9-w8ZMiwaPhnYVWNQcUiE.jpg?auto=webp&s=e7034460cab0303de18cabd7edbf7259a440c244",
-        "message": "Come on down to Paddy's!"      
-        }
-    ]
-    }
+    "floats": ParadeModel().fetch_all()}
     return jsonify(parade), 200
+
+@parade_view.route('/parade/upload', methods=['POST'])
+def parade_upload():
+    parade_id = request.cookies.get('parade_id')
+
+    print(parade_id)
+    if request.method == 'POST':
+        files = request.files['file']
+       
+        try: 
+
+            ParadeModel().change_one({
+                'image': S3Bucket().upload(files),
+                'parade_id': parade_id
+            })
+
+            return redirect('/')
+        except Exception as e:
+
+            print(e)
+
