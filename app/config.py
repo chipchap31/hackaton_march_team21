cee0dc8359ae616e192dcd1c6a63e6c48895fed2
Jomari Alang <jomarialang31@gmail.com>
2021-03-07 01:36:44 +0000

    fix: loaded the flask as production

--- app/config.py
+++ app/config.py
@@ -1,15 +1,15 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DEBUG=True
-ENV='development'
+DEBUG=os.getenv('DEBUG')
+ENV=os.getenv('ENV')
 AWS_ACCESS_KEY=os.getenv('AWS_ACCESS_KEY') 
 AWS_SECRET_KEY=os.getenv('AWS_SECRET_KEY')
 AWS_BUCKET_NAME=os.getenv('AWS_BUCKET_NAME')
 MONGO_URI=os.getenv('MONGO_URI')
 MONGO_DATABASE=os.getenv('MONGO_DATABASE')
 SECRET_KEY=os.getenv('SECRET_KEY')
-
-
 MAIL_SERVER= 'smtp.gmail.com'
 MAIL_PORT= 465
 MAIL_USE_TLS=False
